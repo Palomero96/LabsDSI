6cddf492599d5462787ab93f43de0adf290fa0ca
Palomero96 <31955373+Palomero96@users.noreply.github.com>
2019-03-13 19:12:13 +0100

    microp_5

---
+++ microp_5/Agenda/src/models/contact.model.ts
@@ -0,0 +1,6 @@
+export​ ​interface​ ​Contact​ {
+    ​key​?: ​string​;​//manejará el id cuando se utiliza firebase ​nombre​: ​string​;
+    ​organizacion​: ​string​;
+    ​movil​: ​string​;
+    ​correo​: ​string​; 
+}
