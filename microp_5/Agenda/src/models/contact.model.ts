100466f72173427cc1e380de571d857b39df6319
Palomero96 <31955373+Palomero96@users.noreply.github.com>
2019-03-27 11:20:11 +0100

    Archivos inciales microp 7

--- microp_5/Agenda/src/models/contact.model.ts
+++
@@ -1,6 +0,0 @@
-export​ ​interface​ ​Contact​ {
-    ​key​?: ​string​;​//manejará el id cuando se utiliza firebase ​nombre​: ​string​;
-    ​organizacion​: ​string​;
-    ​movil​: ​string​;
-    ​correo​: ​string​; 
-}
